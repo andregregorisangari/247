a81e98ee3eb65906185866c5a58ce74ce7c36dc1
Andre Gregori Sangari <andresangari12@gmail.com>
2023-12-17 10:19:06 +0700

    edit index

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const dotenv = require('dotenv');
 const { Client, MessageAttachment, RichPresence, MessageEmbed } = require('discord.js-selfbot-v13'); // Define Client Constructor And The MessageAttachment Constructor
 const exp = require('express'); // Define The Express Package
-const req = require("node-fetch");
 const s = exp(); // Make New Express Server
+const autoRespond = "Kalo lama balas berarti lagi tidur bang, ini auto respond wkwk";
 const bot = new Client({
   checkUpdate: false
 }); // Define The User
@@ -17,9 +18,9 @@ const ganti = [
   "Text 9",
   "Dan Seterusnya",
 ]; // Ganti Ke Teks Yang Mau Ganti²
-const prefix = "?"
+const prefix = "."
 const textUtama = "Google Cloud Skills Boost 24 Hours"
-const textDua = "{tanggal} {jam} {menit} {bulan}" // Contoh Kalo Pake Bulan, Tanggal, Jam / Menit
+const textDua = "{tanggal} {bulan}" // Contoh Kalo Pake Bulan, Tanggal, Jam / Menit
 const textTiga = "Google Cloud Skills Boost 24 Hours" // Contoh Kalo Ganti-Ganti
 const textEmpat = "Bangkit 24 Hours"
 const type = "LISTENING" // Type :: PLAYING, LISTENING, WATCHING, STREAMING ( Besar Semua )
@@ -29,10 +30,9 @@ const labelButtonSatu = "GCSB Profile"
 const labelButtonDua = "LinkedIn"
 const linkButtonSatu = "https://www.cloudskillsboost.google/public_profiles/8bc875b0-0dc9-4cc8-90ea-1a8bfe1ca8ec"
 const linkButtonDua = "https://www.linkedin.com/in/andregregs/"
-const autoRespond = "Halo, Kenapa Tag Saya?\n- Auto Respond"
 
 s.all('/', (req, res) => { // Build The Express Server
-  res.send("Ready Dek") // Print Text "Ready Dek"
+  res.send("Ready") // Print Text "Ready Dek"
 })
 
 s.listen(8080, () => { // Make The Express Servers Listen To 8080 Port
@@ -48,8 +48,8 @@ bot.on('ready', async () => { // If The User Was Ready
     function dim(m, y) { // Create "dim" Function
       return new Date(y, m, 0).getDate() // Get The Day Count On Specific Month
     }
-    function getOrdinalNum(n) { // Create New Function
-      return n + (n > 0 ? ['th', 'st', 'nd', 'rd'][(n > 3 && n < 21) || n % 10 > 3 ? 0 : n % 10] : ''); // Get The Ordinal Of Number
+    function getOrdinalNum(n) {
+      return n.toString(); // Convert the number to a string without appending ordinal suffix
     }
     const gonta = ganti[Math.floor(Math.random() * ganti.length)]; // Pick Random Value From The Array
     const date = new Date() // Get New Date
@@ -94,7 +94,6 @@ bot.on('ready', async () => { // If The User Was Ready
       .setAssetsSmallText(`DC - ${bot.user.tag}`)
       .setState(`${hasilDua}`) // RPC State
       .setDetails(`${hasilTiga}`) // RPC Details
-      .setStartTimestamp(Date.now()) // RPC Timestamp
       .addButton(`${labelButtonSatu}`, `${linkButtonSatu}`) // Add New Button
       .addButton(`${labelButtonDua}`, `${linkButtonDua}`)
     bot.user.setActivity(pr.toJSON()) // Activate The RPC
@@ -113,16 +112,24 @@ bot.on('messageCreate', async (msg) => {
     .trim()
     .split(/ +/g)
   if (cmd.toLowerCase() == "translate" || cmd.toLowerCase() == "tl") {
-    let arguments = args.join(" ").split(" | ")
-    if (!arguments[0] || !arguments[1]) return msg.reply({ content: "Contoh Command :\n.tl Hello | id" })
+    let arguments = args.join(" ").split(" | ");
+    if (!arguments[0] || !arguments[1]) return msg.reply({ content: "Contoh Command :\n.tl Hello | id" });
+
     const params = new URLSearchParams({
       to: arguments[1].toLowerCase(),
       text: arguments[0]
-    })
-    const results = await req("https://api.popcat.xyz/translate?" + params);
-    const result = await results.json();
-    msg.delete().then(() => msg.channel.send({ content: `${result.translated}` }))
+    });
+
+    try {
+      const response = await fetch("https://api.popcat.xyz/translate?" + params);
+      const result = await response.json();
+
+      msg.delete().then(() => msg.channel.send({ content: `${result.translated}` }));
+    } catch (error) {
+      console.error("Error fetching or parsing translation:", error);
+      msg.reply({ content: "Error fetching or parsing translation." });
+    }
   }
 })
 
-bot.login(process.env.token) // Login To The User
+bot.login(process.env.TOKEN) // Login To The User
